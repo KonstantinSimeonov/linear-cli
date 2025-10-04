625863f93df004972084dc2ea20e2c4f4ef7dbe0
Konstantin Simeonov <kon.simeonov@protonmail.com>
2025-10-04 13:41:51 +0300

    feat(cli): proto arg parsing, subcommands and interactivity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Parser,Subcommand};
+use inquire::Select;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+      Command::Add { description } => println!("Description: {:?}", description),
+      Command::List => println!("list"),
+      Command::Done { id: Some(id) } => println!("Marking as done {:?}", id),
+      Command::Done { id: _ } => {
+        let choice = Select::new("Gimme:", vec!["a", "b", "c"]).prompt().unwrap();
+        println!("Choice is {}", choice)
+      }
+    }
+}
+
+#[derive(Parser)]
+#[command(name = "task")]
+#[command(about = "Cli for interacting with linear", long_about = None)]
+struct Cli {
+  #[command(subcommand)]
+  command: Command
+}
+
+#[derive(Subcommand)]
+enum Command {
+  Add {
+    #[arg(short, long)]
+    description: Option<String>
+  },
+  List,
+  Done {
+    id: Option<usize>
+  }
 }
