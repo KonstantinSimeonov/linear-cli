126697356849fad781e3218644dd0e38a4aab530
Konstantin Simeonov <kon.simeonov@protonmail.com>
2025-10-17 19:47:17 +0300

    improve(project-list): add count param and errors

--- src/project/mod.rs
+++ src/project/mod.rs
@@ -1,29 +1,61 @@
-use clap::{Subcommand};
+use clap::Subcommand;
 use colored::Colorize;
 
-use crate::{cli_config::LrConfig, exec::Execute, graphql::{blocking_request::gql_request, queries::{projects, Projects}}};
+use crate::{
+    cli_config::LrConfig,
+    exec::Execute,
+    graphql::{
+        blocking_request::gql_request,
+        queries::{
+            projects::{self, ProjectsProjectsNodes},
+            Projects,
+        },
+    },
+};
 
 #[derive(Subcommand)]
 pub enum ProjectCommand {
-  List,
-  View
+    List {
+        #[arg(short, long, default_missing_value = "always", default_value_t = 20)]
+        count: i64,
+    },
+    View,
 }
 
 impl Execute for ProjectCommand {
     fn execute(&self, config: &LrConfig) {
         match self {
             ProjectCommand::View { .. } => unimplemented!(),
-            ProjectCommand::List { .. } => {
-              let response = gql_request::<Projects>(config, projects::Variables {
-                first: Some(20),
-                after: None
-              }).unwrap();
+            ProjectCommand::List { count } => {
+                let projects = gql_request::<Projects>(
+                    config,
+                    projects::Variables {
+                        first: Some(*count),
+                        after: None,
+                    },
+                )
+                .expect("Failed to get projects")
+                .projects
+                .nodes;
 
-              for node in response.projects.nodes.iter() {
-                let lead = node.lead.as_ref().map(|lead| lead.name.clone()).unwrap_or("<None>".to_string());
-                println!("{} [{}] Lead: {}", node.name.bold().blue(), node.status.name, lead.yellow())
-              }
+                for project in projects.iter() {
+                    render_project(project);
+                }
             }
         }
     }
+}
+
+fn render_project(project: &ProjectsProjectsNodes) {
+    let lead = project
+        .lead
+        .as_ref()
+        .map(|lead| lead.name.clone())
+        .unwrap_or("<None>".to_string());
+    println!(
+        "{} [{}] Lead: {}",
+        project.name.bold().blue(),
+        project.status.name,
+        lead.yellow()
+    )
 }
