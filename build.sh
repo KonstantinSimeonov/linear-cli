3823cfc4da6eca2ca90e66a69fc85780df2453bb
Konstantin Simeonov <kon.simeonov@protonmail.com>
2025-10-17 18:56:44 +0300

    feat(shell): add shell completion

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cargo build
-cp target/debug/linear-cli ~/scripts/_lr
--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,11 @@
+use crate::exec::Execute;
 use crate::project::ProjectCommand;
 use crate::{cli_config::LrConfig, issue::IssueCommand};
-use crate::exec::Execute;
 
-use clap::{Parser, Subcommand};
+use clap::{Args, CommandFactory, Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "task")]
+#[command(name = "lr")]
 #[command(about = "Cli for interacting with linear", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
@@ -22,14 +22,28 @@ pub enum Command {
 
     #[command(subcommand)]
     Login,
+
+    #[command(version, about)]
+    Completion(CompletionArgs),
+}
+
+#[derive(Args)]
+pub struct CompletionArgs {
+    #[arg(value_enum)]
+    shell: clap_complete::Shell,
 }
 
 impl Execute for Command {
-  fn execute(&self, config: &LrConfig) {
-      match self {
-          Command::Login => unimplemented!(),
-          Command::Project(cmd) => cmd.execute(config),
-          Command::Issue(cmd) => cmd.execute(config),
-      }
-  }
+    fn execute(&self, config: &LrConfig) {
+        match self {
+            Command::Login => unimplemented!(),
+            Command::Project(cmd) => cmd.execute(config),
+            Command::Issue(cmd) => cmd.execute(config),
+            Command::Completion(CompletionArgs { shell }) => {
+                let mut cmd = Cli::command();
+                let bin_name = cmd.get_name().to_string();
+                clap_complete::generate(*shell, &mut cmd, bin_name, &mut std::io::stdout());
+            }
+        }
+    }
 }
